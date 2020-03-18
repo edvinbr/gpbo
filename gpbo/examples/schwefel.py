4b113eda7566a9380046a3b7d0181f7402ef2c74
Harald <bharald@student.chalmers.se>
2020-03-18 15:43:48 +0100

    Schwefel

---
+++ gpbo/examples/schwefel.py
@@ -0,0 +1,63 @@
+import gpbo
+import scipy as sp
+import numpy as np
+import os
+from datetime import datetime
+import sys
+
+try:
+    suffix = sys.argv[1]
+except IndexError:
+    suffix = ''
+gpbo.core.debugoutput['pathsuffix'] = suffix
+
+
+# dimensionality
+D = 2
+# noise variance
+s = 0.
+# number of step to take
+n = 200
+
+
+#define a simple 2d objective in x which also varies with respect to the environmental variable
+def f(x,**ev):
+    # Scale axes, 
+    z = [xi*500 for xi in x]
+    sum1 = 0
+    for i in range(0,D):
+        sum1 += z[i] * np.sin(np.sqrt(np.abs(z[i])))
+    y = 418.9829*D - sum1
+    y = sp.log(y -(0) + 1)
+    # fixed cost
+    c = 1.
+    # noise
+    n = sp.random.normal() * s
+    # we want to check the noiseless value when evaluating performance
+    if 'cheattrue' in ev.keys():
+        if ev['cheattrue']:
+            n = 0
+    print('f inputs x:{} ev:{} outputs y:{} (n:{}) c:{}'.format(x, ev, y + n, n, c))
+    return y + n, c, dict()
+
+
+timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+#arguments to generate default config are objective function, dimensionality,number of initialization points, number of steps, noise variance, result directory and result filename
+C=gpbo.core.config.switchdefault(f,D,10,n,s,'results','2dschwefel.csv')
+#C = gpbo.core.config.switchetest(f, D, 10, n, s, 'results', 'michalewicz.csv')
+
+# set the target global regret
+C.choosepara['regretswitch'] = 1e-2
+C.choosepara['pvetol'] = 1e-2
+C.aqpara[1]['tol']=None#1e-6
+
+
+print("before search")
+# Add namesuffix as argument to use different savefiles
+initdata = False
+if initdata:
+    C.choosepara = (np.load(os.path.join(gpbo.core.debugoutput['path'], "choosepara"+gpbo.core.debugoutput['pathsuffix']+".npy"),allow_pickle=True)).tolist()
+    #C = (np.load(os.path.join(gpbo.core.debugoutput['path'], "optconfig.npy"),allow_pickle=True)).tolist()
+out = gpbo.search(C, initdata)
+print(out)
+
