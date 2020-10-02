afdff1710f1b87cb243e08966cda1843724b8293
Harald Brorsson <bharald@student.chalmers.se>
2020-10-02 11:20:38 +0200

    noise in test.py

--- gpbo/examples/test.py
+++ gpbo/examples/test.py
@@ -65,7 +65,10 @@ initial_state = 1.0/np.sqrt(nbr_states)*np.ones((nbr_states, 1))
 
 # compute the state for a variational angle for a certain level of p
 # the dimension of the expectation value function is 2*p
-p = 1
+p = 2
+
+#noise
+s = 0
 
 def f(beta_gamma_angles,**ev):
     gamma = [(b+1)/2*2*np.pi for b in beta_gamma_angles[:p]]
@@ -73,7 +76,10 @@ def f(beta_gamma_angles,**ev):
     c = 1.
 
     E = expectation_value((gamma+beta), H, p, nbr_of_qubits, initial_state,sigmax)
-    return E, c, dict()
+
+    n = sp.random.normal() * s
+
+    return E+n, c, dict()
 
     
 #beta = np.pi/2
@@ -81,10 +87,8 @@ def f(beta_gamma_angles,**ev):
 #beta_gamma_angles = np.array([gamma, beta])
 #print(beta_gamma_angles)
 
-#noise
-s = 0
 
-C = gpbo.core.config.switchdefault(f, p*2, 10, 500*(p+1), s, 'results', 'qaoa-instance_8_0_sigma5_1.csv')
+C = gpbo.core.config.switchdefault(f, p*2, 10, 250, s, 'results', 'qaoa-instance_8_0_n_test_p_4_4.csv')
 C.choosepara['regretswitch'] = 1e-2
 C.choosepara['pvetol'] = 1e-2
 C.aqpara[1]['tol']=None
